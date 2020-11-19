2529ccdc676d564f7a14dc47d732ada85d9acdb5
Peter Ho <PupilStudio@outlook.com>
2020-11-19 22:22:09 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace XRepeater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextPath = new System.Windows.Forms.TextBox();
+            this.ButtonLoad = new System.Windows.Forms.Button();
+            this.ButtonPlay = new System.Windows.Forms.Button();
+            this.ButtonAddPt = new System.Windows.Forms.Button();
+            this.ListPts = new System.Windows.Forms.ListView();
+            this.ButtonJmpPt = new System.Windows.Forms.Button();
+            this.ButtonNextSeg = new System.Windows.Forms.Button();
+            this.LabelSeg = new System.Windows.Forms.Label();
+            this.LabelCurTime = new System.Windows.Forms.Label();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.ButtonRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextPath
+            // 
+            this.TextPath.Location = new System.Drawing.Point(12, 12);
+            this.TextPath.Name = "TextPath";
+            this.TextPath.Size = new System.Drawing.Size(433, 21);
+            this.TextPath.TabIndex = 1;
+            // 
+            // ButtonLoad
+            // 
+            this.ButtonLoad.Location = new System.Drawing.Point(451, 10);
+            this.ButtonLoad.Name = "ButtonLoad";
+            this.ButtonLoad.Size = new System.Drawing.Size(75, 23);
+            this.ButtonLoad.TabIndex = 2;
+            this.ButtonLoad.Text = "Load";
+            this.ButtonLoad.UseVisualStyleBackColor = true;
+            this.ButtonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
+            // ButtonPlay
+            // 
+            this.ButtonPlay.Location = new System.Drawing.Point(12, 39);
+            this.ButtonPlay.Name = "ButtonPlay";
+            this.ButtonPlay.Size = new System.Drawing.Size(75, 23);
+            this.ButtonPlay.TabIndex = 3;
+            this.ButtonPlay.Text = "Play";
+            this.ButtonPlay.UseVisualStyleBackColor = true;
+            this.ButtonPlay.Click += new System.EventHandler(this.ButtonPlay_Click);
+            // 
+            // ButtonAddPt
+            // 
+            this.ButtonAddPt.Location = new System.Drawing.Point(93, 39);
+            this.ButtonAddPt.Name = "ButtonAddPt";
+            this.ButtonAddPt.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAddPt.TabIndex = 4;
+            this.ButtonAddPt.Text = "AddPt";
+            this.ButtonAddPt.UseVisualStyleBackColor = true;
+            this.ButtonAddPt.Click += new System.EventHandler(this.ButtonAddPt_Click);
+            // 
+            // ListPts
+            // 
+            this.ListPts.HideSelection = false;
+            this.ListPts.Location = new System.Drawing.Point(12, 68);
+            this.ListPts.MultiSelect = false;
+            this.ListPts.Name = "ListPts";
+            this.ListPts.Size = new System.Drawing.Size(514, 130);
+            this.ListPts.TabIndex = 5;
+            this.ListPts.UseCompatibleStateImageBehavior = false;
+            this.ListPts.View = System.Windows.Forms.View.List;
+            // 
+            // ButtonJmpPt
+            // 
+            this.ButtonJmpPt.Location = new System.Drawing.Point(174, 40);
+            this.ButtonJmpPt.Name = "ButtonJmpPt";
+            this.ButtonJmpPt.Size = new System.Drawing.Size(75, 23);
+            this.ButtonJmpPt.TabIndex = 6;
+            this.ButtonJmpPt.Text = "JmpPt";
+            this.ButtonJmpPt.UseVisualStyleBackColor = true;
+            this.ButtonJmpPt.Click += new System.EventHandler(this.ButtonJmpPt_Click);
+            // 
+            // ButtonNextSeg
+            // 
+            this.ButtonNextSeg.Location = new System.Drawing.Point(255, 40);
+            this.ButtonNextSeg.Name = "ButtonNextSeg";
+            this.ButtonNextSeg.Size = new System.Drawing.Size(75, 23);
+            this.ButtonNextSeg.TabIndex = 7;
+            this.ButtonNextSeg.Text = "NextSeg";
+            this.ButtonNextSeg.UseVisualStyleBackColor = true;
+            this.ButtonNextSeg.Click += new System.EventHandler(this.ButtonNextSeg_Click);
+            // 
+            // LabelSeg
+            // 
+            this.LabelSeg.AutoSize = true;
+            this.LabelSeg.Location = new System.Drawing.Point(339, 44);
+            this.LabelSeg.Name = "LabelSeg";
+            this.LabelSeg.Size = new System.Drawing.Size(65, 12);
+            this.LabelSeg.TabIndex = 8;
+            this.LabelSeg.Text = "CurSeg: -1";
+            // 
+            // LabelCurTime
+            // 
+            this.LabelCurTime.AutoSize = true;
+            this.LabelCurTime.Location = new System.Drawing.Point(410, 44);
+            this.LabelCurTime.Name = "LabelCurTime";
+            this.LabelCurTime.Size = new System.Drawing.Size(71, 12);
+            this.LabelCurTime.TabIndex = 9;
+            this.LabelCurTime.Text = "CurTime: -1";
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(12, 204);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStop.TabIndex = 10;
+            this.ButtonStop.Text = "Stop";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // ButtonRemove
+            // 
+            this.ButtonRemove.Location = new System.Drawing.Point(93, 204);
+            this.ButtonRemove.Name = "ButtonRemove";
+            this.ButtonRemove.Size = new System.Drawing.Size(75, 23);
+            this.ButtonRemove.TabIndex = 11;
+            this.ButtonRemove.Text = "RemovePt";
+            this.ButtonRemove.UseVisualStyleBackColor = true;
+            this.ButtonRemove.Click += new System.EventHandler(this.ButtonRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 233);
+            this.Controls.Add(this.ButtonRemove);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.LabelCurTime);
+            this.Controls.Add(this.LabelSeg);
+            this.Controls.Add(this.ButtonNextSeg);
+            this.Controls.Add(this.ButtonJmpPt);
+            this.Controls.Add(this.ListPts);
+            this.Controls.Add(this.ButtonAddPt);
+            this.Controls.Add(this.ButtonPlay);
+            this.Controls.Add(this.ButtonLoad);
+            this.Controls.Add(this.TextPath);
+            this.Name = "Form1";
+            this.Text = "Repeater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TextPath;
+        private System.Windows.Forms.Button ButtonLoad;
+        private System.Windows.Forms.Button ButtonPlay;
+        private System.Windows.Forms.Button ButtonAddPt;
+        private System.Windows.Forms.ListView ListPts;
+        private System.Windows.Forms.Button ButtonJmpPt;
+        private System.Windows.Forms.Button ButtonNextSeg;
+        private System.Windows.Forms.Label LabelSeg;
+        private System.Windows.Forms.Label LabelCurTime;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.Button ButtonRemove;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WMPLib;
+using AxWMPLib;
+
+namespace XRepeater
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        List<double> pts;
+        int curseg;
+        WindowsMediaPlayerClass wmp1 = new WindowsMediaPlayerClass();
+        bool waitForOpen;
+        bool playing = false;
+        Timer refreshTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fmtTime(double dt)
+        {
+            long t = (long)dt;
+            return (t / 60).ToString() + ":" + (t % 60).ToString();
+        }
+        void RefreshUI()
+        {
+            this.ListPts.Clear();
+            pts.Sort();
+            foreach (var i in pts)
+                ListPts.Items.Add(fmtTime(i));
+            this.LabelSeg.Text = "CurSeg: " + curseg.ToString() ;
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            wmp1 = new WindowsMediaPlayerClass();
+            wmp1.settings.autoStart = false;
+            wmp1.URL = TextPath.Text.Trim();
+            wmp1.play();
+            waitForOpen = true;
+            wmp1.OpenStateChange += (state) =>
+            {
+                if (waitForOpen)
+                {
+                    waitForOpen = false;
+                    pts = new List<double>();
+                    pts.Add(0);
+                    pts.Add(wmp1.currentMedia.duration);
+                    curseg = 1;
+                    RefreshUI();
+                    //MessageBox.Show(wmp1.currentMedia.durationString);
+                    wmp1.stop();
+                    refreshTimer = new Timer();
+                    refreshTimer.Enabled = true;
+                    refreshTimer.Interval = 500;
+                    refreshTimer.Tick += (ss, ee) =>
+                    {
+                        if (wmp1.controls.currentPosition >= pts[curseg])
+                        {
+                            wmp1.controls.currentPosition = pts[curseg - 1];
+                        }
+                        LabelCurTime.Text = "CurTime: " + wmp1.currentPositionString;
+                        
+                    };
+                }
+            };
+
+        }
+
+        private void ButtonPlay_Click(object sender, EventArgs e)
+        {
+            if (playing)
+            {
+                // pause
+                wmp1.controls.pause();
+                ButtonPlay.Text = "Play";
+            } else
+            {
+                // play
+                wmp1.controls.play();
+                ButtonPlay.Text = "Pause";
+            }
+            playing = !playing;
+        }
+
+        private void ButtonAddPt_Click(object sender, EventArgs e)
+        {
+            pts.Add(wmp1.currentPosition);
+            ++curseg;
+            RefreshUI();
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            playing = false;
+            wmp1.stop();
+            curseg = 1;
+            ButtonPlay.Text = "Play";
+        }
+
+        private void ButtonNextSeg_Click(object sender, EventArgs e)
+        {
+            ++curseg;
+            if (curseg == pts.Count)
+                curseg = 1;
+            wmp1.currentPosition = pts[curseg - 1];
+        }
+
+        private void ButtonJmpPt_Click(object sender, EventArgs e)
+        {
+            if (ListPts.SelectedItems.Count == 0) return;
+            curseg = ListPts.SelectedItems[0].Index + 1;
+            if (curseg == pts.Count)
+                --curseg;
+            wmp1.currentPosition = pts[curseg - 1];
+        }
+
+
+        private void ButtonRemove_Click(object sender, EventArgs e)
+        {
+            if (ListPts.SelectedItems.Count == 0) return;
+            if (pts.Count == 2) return;
+            if (ListPts.SelectedItems[0].Index == 0
+                || ListPts.SelectedItems[0].Index == pts.Count - 1) return;
+
+            if (ListPts.SelectedItems[0].Index == curseg)
+            {
+                // do nothing
+            }
+            else if (ListPts.SelectedItems[0].Index == curseg - 1)
+            {
+                --curseg;
+            }
+            pts.RemoveAt(ListPts.SelectedItems[0].Index);
+            RefreshUI();
+        }
+    }
+}
